2f7c185c9ac268c52a21f9a129640a3015381aa1
Andrius Mudinas <andrius@wikiled.com>
2018-11-29 18:24:50 +0000

    Adding logging module

---
+++ src/Wikiled.Common.Utilities/Helpers/LoggingModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Wikiled.Common.Utilities.Helpers
+{
+    public class LoggingModule : Module
+    {
+        private readonly IServiceCollection services = new ServiceCollection();
+
+        public LoggingModule(ILoggerFactory factory)
+        {
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
+
+            services.AddSingleton(factory);
+            services.AddLogging(logBuilder =>
+            {
+                logBuilder.SetMinimumLevel(LogLevel.Trace);
+            });
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Populate(services);
+        }
+    }
+}
