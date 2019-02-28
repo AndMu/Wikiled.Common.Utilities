bcb16c380405a438cd29f1e7ad13698b2bbdcc26
Andrius Mudinas <andrius@wikiled.com>
2019-02-28 15:06:02 +0000

    Adding auth client interface

---
+++ src/Wikiled.Common.Utilities/Auth/IAuthClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Wikiled.Common.Utilities.Auth
+{
+    public interface IAuthClient<T>
+        where T : class
+    {
+        string BuildAuthorizeUrl(string callback = null);
+
+        Task<T> GetToken(string code, string redirectUri = null);
+
+        Task<T> RefreshToken(T token);
+    }
+}
