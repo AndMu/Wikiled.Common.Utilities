1141e14e912f11706ff3904bf55a6db8e1d387be
Andrius Mudinas <andrius@wikiled.com>
2019-06-11 11:16:01 +0100

    Moving OAuth to this projecct

---
+++ src/Wikiled.Common.Utilities/Auth/OAuth/OAuthAuthentication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Wikiled.Common.Utilities.Auth.OAuth
+{
+    public class OAuthAuthentication<T> : IAuthentication<T>
+        where T : class
+    {
+        private readonly IAuthClient<T> client;
+
+        private readonly IOAuthHelper helper;
+
+        public OAuthAuthentication(IAuthClient<T> client, IOAuthHelper helper)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+            this.helper = helper ?? throw new ArgumentNullException(nameof(helper));
+        }
+
+        public async Task<T> Authenticate()
+        {
+            var auth = client.BuildAuthorizeUrl(helper.RedirectUri);
+            await helper.Start(auth, null).ConfigureAwait(false);
+            var code = helper.Code;
+            T token = await client.GetToken(code, helper.RedirectUri).ConfigureAwait(false);
+            return token;
+        }
+
+        public Task<T> Refresh(T old)
+        {
+            return client.RefreshToken(old);
+        }
+    }
+}
