aef996cfc18c8ee16bf9b08744e68604c5dfa6f6
Andrius Mudinas <andrius@wikiled.com>
2019-06-11 10:58:34 +0100

    Exposing inteface

---
+++ src/Wikiled.Common.Utilities/Auth/IEncryptor.cs
@@ -0,0 +1,11 @@
+﻿namespace Wikiled.Common.Utilities.Auth
+{
+    public interface IEncryptor
+    {
+        string Salt { get; }
+
+        string EncryptString(string plainText, string passPhrase);
+
+        string DecryptString(string cipherText, string passPhrase);
+    }
+}
