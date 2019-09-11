dca6181abcc408d9febebc3839e7b5f5fff9ba55
Andrius Mudinas <keistokas@gmail.com>
2019-09-11 12:03:14 +0100

    Adding named instances

---
+++ src/Wikiled.Common.Utilities/Modules/IServiceMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Wikiled.Common.Utilities.Modules
+{
+    internal interface IServiceMetadata<out TService, out TMetadata>
+    {
+        Func<IServiceProvider, TService> CachingImplementationFactory { get; }
+
+        TMetadata Metadata { get; }
+    }
+
+    internal interface IServiceMetadata<out TService, out TImplementation, out TMetadata> : IServiceMetadata<TService, TMetadata>
+        where TService : class
+        where TImplementation : class, TService
+    {
+        new Func<IServiceProvider, TImplementation> CachingImplementationFactory { get; }
+    }
+}
---
+++ src/Wikiled.Common.Utilities/Modules/ServiceMetadata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Wikiled.Common.Utilities.Modules
+{
+    internal class ServiceMetadata<TService, TImplementation, TMetadata> : IServiceMetadata<TService, TImplementation, TMetadata>
+        where TService : class
+        where TImplementation : class, TService
+    {
+
+        private TImplementation cachedImplementation;
+
+        Func<IServiceProvider, TService> IServiceMetadata<TService, TMetadata>.CachingImplementationFactory => CachingImplementationFactory;
+
+        public Func<IServiceProvider, TImplementation> CachingImplementationFactory { get; }
+
+        public TMetadata Metadata { get; }
+
+        public ServiceMetadata(TMetadata metadata, Func<IServiceProvider, TImplementation> implementationFactory)
+        {
+            if (implementationFactory == null) throw new ArgumentNullException(nameof(implementationFactory));
+            Metadata = metadata;
+            CachingImplementationFactory = s => cachedImplementation ?? (cachedImplementation = implementationFactory(s));
+        }
+    }
+}
