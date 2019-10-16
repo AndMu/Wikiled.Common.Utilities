cd1f138e67c2706014ca8876283f635103b9c092
Andrius Mudinas <keistokas@gmail.com>
2019-10-16 16:31:46 +0100

    Adding testing utilities

---
+++ src/Wikiled.Common.Testing.Utilities/Reflection/ConstructorHelper.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Wikiled.Common.Testing.Utilities.Reflection
+{
+    public static class ConstructorHelper
+    {
+        public static void ConstructorMustThrowArgumentNullException(Type type, params object[] args)
+        {
+            var lookup = args.ToLookup(item => item.GetType(), item => item);
+
+            foreach (var constructor in type.GetConstructors())
+            {
+                var parameters = constructor.GetParameters();
+
+                var arguments = parameters.Select(
+                                              p =>
+                                              {
+                                                  if (lookup.Contains(p.ParameterType))
+                                                  {
+                                                      return lookup[p.ParameterType].First();
+                                                  }
+
+                                                  if (p.ParameterType.IsEnum)
+                                                  {
+                                                      return p.ParameterType.GetEnumValues().GetValue(0);
+                                                  }
+
+                                                  Type mockType = typeof(Mock<>).MakeGenericType(p.ParameterType);
+                                                  return ((Mock) Activator.CreateInstance(mockType)).Object;
+                                              })
+                                          .ToArray();
+
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    var mocksCopy = arguments.ToArray();
+                    mocksCopy[i] = null;
+
+                    try
+                    {
+                        constructor.Invoke(mocksCopy);
+                        Assert.Fail("ArgumentNullException expected for parameter {0} of constructor, but no exception was thrown",
+                                    parameters[i].Name);
+                    }
+                    catch (TargetInvocationException ex)
+                    {
+                        Assert.AreEqual(typeof(ArgumentNullException),
+                                        ex.InnerException?.GetType(),
+                                        $"ArgumentNullException expected for parameter {parameters[i].Name} of  constructor, but exception of type {ex.InnerException.GetType()} was thrown");
+                    }
+                }
+            }
+        }
+    }
+}
