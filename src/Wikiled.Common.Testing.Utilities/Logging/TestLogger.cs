17646c49c35ce0e8ddeaea10f10b1695824ac9df
Andrius Mudinas <keistokas@gmail.com>
2019-10-16 17:09:22 +0100

    Adding logger

---
+++ src/Wikiled.Common.Testing.Utilities/Logging/TestLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Wikiled.Common.Testing.Utilities.Logging
+{
+    public static class TestLogger
+    {
+        public static ILogger<T> Create<T>()
+        {
+            var logger = new NUnitLogger<T>();
+            return logger;
+        }
+    }
+}
