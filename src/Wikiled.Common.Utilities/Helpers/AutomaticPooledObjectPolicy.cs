3e45b40ff21de76aaa616d430f873c523ececbc6
Andrius Mudinas <keistokas@gmail.com>
2019-11-01 21:04:54 +0000

    Delegate pool policy

---
+++ src/Wikiled.Common.Utilities/Helpers/AutomaticPooledObjectPolicy.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
+
+namespace Wikiled.Common.Utilities.Helpers
+{
+    public class AutomaticPooledObjectPolicy<T> : IPooledObjectPolicy<T>
+    {
+        private readonly Func<T> factory;
+
+        private readonly Func<T, bool> clean;
+
+        public AutomaticPooledObjectPolicy(Func<T> factory, Func<T, bool> clean)
+        {
+            this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            this.clean = clean ?? throw new ArgumentNullException(nameof(clean));
+        }
+
+        public T Create()
+        {
+            return factory();
+        }
+
+        public bool Return(T obj)
+        {
+            return clean(obj);
+        }
+    }
+}
