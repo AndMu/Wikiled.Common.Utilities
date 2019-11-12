f2b637a4b6f17060250a640220205b774c310420
Andrius Mudinas <keistokas@gmail.com>
2019-11-12 17:24:46 +0000

    Adding Excetions

---
+++ src/Wikiled.Common.Utilities/Helpers/TaskExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Wikiled.Common.Utilities.Helpers
+{
+    public static class TaskExtensions
+    {
+        public static void ForgetOrThrow(this Task task, ILogger logger)
+        {
+            task.ContinueWith(t => { logger.LogError(t.Exception, "Error"); }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+    }
+}
