56d693f67256f9c6c886a57854879819da330354
Andrius Mudinas <keistokas@gmail.com>
2020-02-05 11:56:09 +0000

    Adding service factory

---
+++ src/Wikiled.Common.Utilities/Modules/ContainerServiceFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Wikiled.Common.Utilities.Modules
+{
+    internal class ContainerServiceFactory<T> : IServiceFactory<T>
+    {
+        readonly IServiceProvider services;
+
+        public ContainerServiceFactory(IServiceProvider services) => this.services = services;
+
+        public T GetService() => services.GetRequiredService<T>();
+    }
+}
---
+++ src/Wikiled.Common.Utilities/Modules/IServiceFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Wikiled.Common.Utilities.Modules
+{
+    public interface IServiceFactory<out T>
+    {
+        T GetService();
+    }
+}
