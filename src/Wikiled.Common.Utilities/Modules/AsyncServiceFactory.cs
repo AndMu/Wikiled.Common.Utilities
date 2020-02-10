f44c8da8c67315107b46010623bd9e6608c19306
Andrius Mudinas <keistokas@gmail.com>
2020-02-10 10:52:23 +0000

    Improve factory

--- src/Wikiled.Common.Utilities/Modules/AsyncServiceFactory.cs
+++ src/Wikiled.Common.Utilities/Modules/AsyncServiceFactory.cs
@@ -6,19 +6,19 @@ namespace Wikiled.Common.Utilities.Modules
 {
     public class AsyncServiceFactory<T> : IAsyncServiceFactory<T>
     {
-        readonly IServiceProvider services;
-        private readonly Func<T, Task> init;
+        readonly IServiceProvider provider;
+        private readonly Func<IServiceProvider, T, Task> init;
 
-        public AsyncServiceFactory(IServiceProvider services, Func<T, Task> init)
+        public AsyncServiceFactory(IServiceProvider services, Func<IServiceProvider, T, Task> init)
         {
-            this.services = services ?? throw new ArgumentNullException(nameof(services));
+            this.provider = services ?? throw new ArgumentNullException(nameof(services));
             this.init = init ?? throw new ArgumentNullException(nameof(init));
         }
 
         public async Task<T> GetService()
         {
-            var service = services.GetRequiredService<T>();
-            await init(service).ConfigureAwait(false);
+            var service = provider.GetRequiredService<T>();
+            await init(provider, service).ConfigureAwait(false);
             return service;
         }
     }
