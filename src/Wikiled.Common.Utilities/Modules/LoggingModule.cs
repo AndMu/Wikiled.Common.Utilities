9873b1534d1b9f043992a6ebe7a97c1cc280c351
Andrius Mudinas <keistokas@gmail.com>
2020-02-18 00:46:40 +0000

    Adding external build action

--- src/Wikiled.Common.Utilities/Modules/LoggingModule.cs
+++ src/Wikiled.Common.Utilities/Modules/LoggingModule.cs
@@ -10,27 +10,25 @@ namespace Wikiled.Common.Utilities.Modules
     {
         private readonly ILoggerFactory factory;
 
+        private readonly Action<ILoggingBuilder> buildAction;
+
         public LoggingModule()
             : this(new WrappedLoggerFactory(ApplicationLogging.LoggerFactory))
         {
         }
 
-        public LoggingModule(ILoggerFactory factory)
+        public LoggingModule(ILoggerFactory factory, Action<ILoggingBuilder> action = null)
         {
             this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            buildAction = action ?? (logBuilder => { logBuilder.SetMinimumLevel(LogLevel.Trace); });
         }
 
         public IServiceCollection ConfigureServices(IServiceCollection services)
         {
             var logger = factory.CreateLogger<LoggingModule>();
             services.AddSingleton(factory);
-            services.AddLogging(logBuilder =>
-            {
-                logBuilder.SetMinimumLevel(LogLevel.Trace);
-            });
-
+            services.AddLogging(buildAction);
             logger.LogDebug("Setting logging module");
-
             return services;
         }
     }
